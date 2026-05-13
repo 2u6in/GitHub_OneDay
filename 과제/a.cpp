0d62a5596beb56612e5726ec695228c8b4faaa36
2u6in <zxcv35602667@gmail.com>
2026-05-13 17:13:06 +0900

    aa

---
+++ 과제/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using ll = long long;
+using pp = pair<int,int>;
+
+const ll INF = (ll)1e18;
+#define MAX 1e9
+const int MOD = 1000000007;
+
+int main(){
+    fastio;
+
+
+    return 0;
+}
